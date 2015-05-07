232b980f43e4fddbfda7f8bc9c457a4fd0676e4a
Jianjun Zhang <zhangjianjun@bubi.cn>
2015-05-07 23:37:54 +0800

    ledger database code complete, more

--- src/database/sqlite_imp.cpp
+++ src/database/sqlite_imp.cpp
@@ -1,8 +1,12 @@
 #include "sqlite_imp.h"
+#include "utils.h"
+
+#include <assert.h>
+
 
 namespace Bubi {
 
-std::string	SqliteInstance::db_name_;
+std::string	SqliteInstance::db_name_ = "";
 SqliteImp::pointer	SqliteInstance::sqlite_ptr_ = nullptr;
 std::mutex	SqliteInstance::sqlite_mutex_;
 
@@ -11,18 +15,21 @@ SqliteInstance::instance (){
 	std::unique_lock<std::mutex> lock (sqlite_mutex_);
 	if (sqlite_ptr_)
 		return sqlite_ptr_;
+	printf ("first time!!!\n");
 	sqlite_ptr_ = std::make_shared <SqliteImp> (db_name_);
 	return sqlite_ptr_;
 }
 
-SqliteImp::SqliteImp (std::string& name){
+SqliteImp::SqliteImp (std::string name)
+:db_name_ (name) {
 	int ret;
-	ret = sqlite3_open (name, &db_);
+	ret = sqlite3_open (db_name_.c_str(), &db_);
 	assert (ret == 0);
 	BUBI_LOG ("sqlite database open success");
 }
 
 SqliteImp::~SqliteImp (){
+	printf ("sqlite db close\n");
 	sqlite3_close (db_);	
 }
 
@@ -30,7 +37,7 @@ int
 SqliteImp::exec_sql (std::string& sql, Callback f){
 	int ret;
 	char *err = 0;
-	ret = sqlite3_exec (db_, sql, f, 0, &err);
+	ret = sqlite3_exec (db_, sql.c_str(), f, 0, &err);
 	if (ret != SQLITE_OK)
 		return 1;
 	else
--- src/database/sqlite_imp.h
+++ src/database/sqlite_imp.h
@@ -2,21 +2,40 @@
 #define _BUBI_SQLITE_IMP_H_
 
 #include <sqlite3.h>
+#include <string.h>
+#include <memory>
+#include <mutex>
 
 namespace Bubi {
 
+typedef int (* Callback)(void *, int, char**, char**);
+
 class SqliteImp {
 public:
+	typedef std::shared_ptr<SqliteImp>	pointer;
+
+	SqliteImp (std::string name);
+	~SqliteImp ();
+
+	int exec_sql (std::string& sql, Callback f);
 
 private:
 	sqlite3* db_;
+	std::string db_name_;
 };
 
 class SqliteInstance {
 public:
+	static void set_db_name (std::string& _name){
+		db_name_ = _name;
+	}
+	static SqliteImp::pointer instance ();
 
 private:
-
+	SqliteInstance () {}
+	static	std::string			db_name_;
+	static	SqliteImp::pointer	sqlite_ptr_;
+	static	std::mutex			sqlite_mutex_;
 };
 
 }
